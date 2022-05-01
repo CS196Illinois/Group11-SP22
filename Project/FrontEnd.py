7b4da8819a68a4377bf583ec88f59422002c0469
justink6 <justink6@illinois.edu>
2022-05-01 10:31:47 -0500

    cleanup

--- Project/FrontEnd.py
+++ Project/FrontEnd.py
@@ -10,31 +10,6 @@ import matplotlib.pyplot as plt
 from tkinter import *
 from Backend import *
 import csv
-
-
-
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
-
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
-
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-        
-        self.test_button = Button(master, text = "Test Zoey", command = self.Zoey)
-        self.test_button.pack()
-
-    def greet(self):
-        print("Greetings!")
-    def Zoey(self):
-        print("Hello from Zoey")
-
   
 class UserInterface:
     
@@ -45,14 +20,12 @@ class UserInterface:
 
         self.enterGrade_label = Label(master, text = "Open the csv file")
         self.enterGrade_label.pack()
-
        
         self.button = tk.Button(master, text='Open', command=self.UploadAction)
         self.button.pack()
 
         self.enterGrade_label = Label(master, text = "Select the type of curve")
         self.enterGrade_label.pack()
-
 
         self.check_button = Checkbutton(master, text="sqrtCurver")
         self.check_button.pack()
@@ -66,19 +39,6 @@ class UserInterface:
         self.check4_button = Checkbutton(master, text="normalCurver")
         self.check4_button.pack()
 
-    
-
-
-
-
- 
-
-
-
-
-
-
-
     filename = ''
     def UploadAction(self):
         filename = filedialog.askopenfilename()
@@ -89,10 +49,6 @@ class UserInterface:
     
     def sel():
         selection = "You selected the option"
-
-
-  
-
 
 root = tk.Tk()
 my_gui = UserInterface(root)
